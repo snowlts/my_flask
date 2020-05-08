039c28fb329f2ca015f7ece226ddbd334969ab54
snowlts <snowlts@163.com>
2020-05-08 12:43:40 +0800

    config.py change requirementschange

--- config.py
+++ config.py
@@ -73,12 +73,25 @@ class ProductionConfig(Config):
         app.logger.addHandler(mail_handler)
 
 
+class HerokuConfig(ProductionConfig):
+    @classmethod
+    def init_app(cls,app):
+        ProductionConfig.init_app(app)
+
+        #log to stderr
+        import logging
+        from logging import StreamHandler
+        file_handler = StreamHandler()
+        file_handler.setLevel(logging.INFO)
+        app.logger.addHandler(file_handler)
+
 
 
 config = {
     'development': DevelopmentConfig,
     'testing': TestingConfig,
     'production': ProductionConfig,
+    'heroku': HerokuConfig,
 
     'default': DevelopmentConfig
 }
